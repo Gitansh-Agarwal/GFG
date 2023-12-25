ffb796507e8b4522b20bf91a16576b59ba1e4f25
Gitansh Agarwal <109718786+Gitansh-Agarwal@users.noreply.github.com>
2023-12-25 15:51:20 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Determinant of a Matrix/determinant-of-a-matrix.cpp
@@ -0,0 +1,69 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+
+
+class Solution
+{   
+    public:
+    //Function for finding determinant of matrix.
+    int determinantOfMatrix(vector<vector<int> > matrix, int n)
+    {
+        // code here 
+        if(n==1) return matrix[0][0];
+        if(n==2) return matrix[0][0]*matrix[1][1] - matrix[0][1]*matrix[1][0];
+        
+        int ans = 0;
+        int sign = 1;
+        
+        for(int i=0; i<n; i++){
+            
+            vector<vector<int>> subMatrix(n-1, vector<int>(n-1));
+            
+            //Filling the subMatrix column-wise.
+            int row = 0;
+            for(int col=0; col<n; col++){
+                
+                if(col==i)
+                    continue;
+                for(int k=1; k<n; k++){
+                    subMatrix[k-1][row] = matrix[k][col];
+                }
+                row++;
+            }
+            ans+=sign*matrix[0][i]*determinantOfMatrix(subMatrix, n-1);
+            sign = -sign;
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin>>t;
+    
+    while(t--) 
+    {
+        int n;
+        cin>>n;
+        vector<vector<int> > matrix(n); 
+
+        for(int i=0; i<n; i++)
+        {
+            matrix[i].assign(n, 0);
+            for( int j=0; j<n; j++)
+            {
+                cin>>matrix[i][j];
+            }
+        }
+
+        Solution ob;
+        int result = ob.determinantOfMatrix(matrix, n);
+        cout<<result<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
