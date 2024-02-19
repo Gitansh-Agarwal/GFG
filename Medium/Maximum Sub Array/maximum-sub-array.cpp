eaec2763e79e36fdcaa326c465cddaa66b974ec2
Gitansh Agarwal <109718786+Gitansh-Agarwal@users.noreply.github.com>
2024-02-19 17:00:00 +0530

    Updated Solution - GfG to GitHub

--- Medium/Maximum Sub Array/maximum-sub-array.cpp
+++ Medium/Maximum Sub Array/maximum-sub-array.cpp
@@ -9,6 +9,37 @@ class Solution{
 public:
 	vector<int> findSubarray(int a[], int n) {
 	    // code here
+	    //Brute Force Solution
+	    //TC=O(N^2), SC=O(N), extra SC=O(1).
+	   // long long sum=0, maxSum=0;
+	   // vector<int> temp, ans;
+	   // for(int i=0; i<n; i++){
+	   //     if(a[i] < 0){
+	   //         temp.clear();
+	   //         sum=0;
+	   //     }
+	   //     else{
+	   //         sum+=a[i];
+	   //         temp.push_back(a[i]);
+	   //         if(sum > maxSum){
+	   //             maxSum=sum;
+	   //             ans = temp; 
+	   //         }
+	   //         else if(sum == maxSum){
+	   //             if(temp.size() > ans.size()){
+	   //                 ans = temp;
+	   //             }
+	   //         }
+	   //     }
+	   // }
+	   // if(ans.empty())
+	   //     ans.push_back(-1);
+	   // return ans;
+	   
+	    
+	    
+	    
+	    //TC=O(N), SC=O(N), extra SC=O(1).
 	    int sum = 0;
 	    int max_sum = INT_MIN;
 	    vector<int> result;
