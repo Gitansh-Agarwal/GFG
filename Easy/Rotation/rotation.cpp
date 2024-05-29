c4f11d10a457e48873e62b0eca5db4bbf10b1b88
Gitansh Agarwal <109718786+Gitansh-Agarwal@users.noreply.github.com>
2024-05-30 00:57:21 +0530

    Updated Solution - GfG to GitHub

--- Easy/Rotation/rotation.cpp
+++ Easy/Rotation/rotation.cpp
@@ -10,13 +10,52 @@ class Solution{
 public:	
 	int findKRotation(int arr[], int n) {
 	    // code here
-	    int count=0;
-	    int i=0;
-	    while(arr[i]<arr[(i+1)%n]){
-	        count++;
-	        i++;
-	    }
-	    return (i+1)%n;
+	    //TC=O(N).
+	   // int count=0;
+	   // int i=0;
+	   // while(arr[i]<arr[(i+1)%n]){
+	   //     count++;
+	   //     i++;
+	   // }
+	   // return (i+1)%n;
+	   
+	   
+	   //TC=O(logn)
+        int low=0;
+        int high=n-1;
+        int ans=INT_MAX;
+        int index=-1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            
+            //search space is already sorted, then arr[low] will always be the smallest in that search space
+            if(arr[low]<=arr[high]){
+                if(arr[low]<ans){
+                    ans=arr[low];
+                    index=low;
+                }
+                break;
+            }
+            
+            //otherwise either one of the left or right half is sorted.
+            //left half is sorted
+            if(arr[low]<=arr[mid]){
+                if(arr[low]<ans){
+                    ans=arr[low];
+                    index=low;
+                }
+                low=mid+1;
+            }            
+            //right half is sorted
+            else if(arr[mid]<=arr[high]){
+                if(arr[mid]<ans){
+                    ans=arr[mid];
+                    index=mid;
+                }
+                high=mid-1;
+            }
+        }
+        return index;
 	}
 
 };
