a00763678f6a2c26c79f1afefb2bb7ef94989e43
Gitansh Agarwal <109718786+Gitansh-Agarwal@users.noreply.github.com>
2024-08-09 19:06:16 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Row with max 1s/row-with-max-1s.cpp
+++ Difficulty: Medium/Row with max 1s/row-with-max-1s.cpp
@@ -7,23 +7,64 @@ using namespace std;
 // User function template for C++
 class Solution {
   public:
+    int lowerBound(vector<int> arr, int n, int x){
+        int low=0;
+        int high=n-1;
+        int ans=n;
+        
+        while(low<=high){
+            int mid=(low+high)/2;
+            
+            if(arr[mid]>=x){
+                ans=mid;
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        return ans;
+    }
     int rowWithMax1s(vector<vector<int> > &arr) {
         // code here
+        //Using Kind of linear search.
+        //TC=O(N*M).
+        //SC=O(1).
+        
+        // int n=arr.size();
+        // int m=arr[0].size();
+        // int maxOnes=0;
+        // int maxOnesRowsInd=-1;
+        // for(int i=0; i<n; i++){
+        //     int cntOne=0;
+        //     for(int j=0; j<m; j++){
+                // if(arr[i][j]==1)   cntOne++;
+                //OR
+                // cntOne+=arr[i][j];
+        //     }
+        //     if(cntOne>maxOnes){
+        //         maxOnes=cntOne;
+        //         maxOnesRowsInd=i;
+        //     }
+        // }
+        // return maxOnesRowsInd;
+        
+        
+        //TC=O(N* log2(M)).
+        //SC=O(1).
         int n=arr.size();
         int m=arr[0].size();
         int maxOnes=0;
-        int maxOnesRowsInd=-1;
+        int index=-1;
         for(int i=0; i<n; i++){
-            int cntOne=0;
-            for(int j=0; j<m; j++){
-                if(arr[i][j]==1)   cntOne++;
-            }
-            if(cntOne>maxOnes){
-                maxOnes=cntOne;
-                maxOnesRowsInd=i;
+            int cntOnes= m - lowerBound(arr[i], m, 1);
+            if(cntOnes>maxOnes){
+                maxOnes=cntOnes;
+                index=i;
             }
         }
-        return maxOnesRowsInd;
+        return index;
+        
     }
 };
 
