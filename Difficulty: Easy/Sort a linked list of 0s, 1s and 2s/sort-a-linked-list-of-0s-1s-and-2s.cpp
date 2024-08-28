8c1a2a7b465425f00d33ca4979aa26e32bc3abfc
Gitansh Agarwal <109718786+Gitansh-Agarwal@users.noreply.github.com>
2024-08-28 10:34:47 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Sort a linked list of 0s, 1s and 2s/sort-a-linked-list-of-0s-1s-and-2s.cpp
+++ Difficulty: Easy/Sort a linked list of 0s, 1s and 2s/sort-a-linked-list-of-0s-1s-and-2s.cpp
@@ -46,34 +46,103 @@ void freeList(struct Node* head) {
 
 */
 class Solution {
+  private:
+    Node* findMiddle(Node* head){
+        Node* slow=head;
+        Node* fast=head;
+        
+        while(fast->next!=nullptr && fast->next->next!=nullptr){
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        return slow;
+    }
+    
+    Node* mergedList(Node* leftHead, Node* rightHead){
+        Node *mergedHead=nullptr, *temp=nullptr;
+        while(leftHead!=nullptr && rightHead!=nullptr){
+            if(leftHead->data < rightHead->data){
+                if(mergedHead==nullptr){
+                    mergedHead=leftHead;
+                    temp=leftHead;
+                }
+                else{
+                    temp->next=leftHead;
+                    temp=temp->next;//temp=leftHead;
+                }
+                leftHead=leftHead->next;
+            }
+            else{
+                if(mergedHead==nullptr){
+                    mergedHead=rightHead;
+                    temp=rightHead;
+                }
+                else{
+                    temp->next=rightHead;
+                    temp=temp->next;
+                }
+                rightHead=rightHead->next;
+            }
+        }
+        
+        if(leftHead==nullptr){
+            temp->next=rightHead;
+        }
+        else{
+            temp->next=leftHead;
+        }
+        
+        return mergedHead;
+    }
   public:
     // Function to sort a linked list of 0s, 1s and 2s.
     Node* segregate(Node* head) {
 
         // Add code here
-        int hash[3]={0};
-        Node* temp=head;
-        while(temp!=nullptr){
-            hash[temp->data]++;
-            temp=temp->next;
+        //Using Extra Space.
+        //TC=O(2*N).
+        //SC=O(3) = O(1).
+        // int hash[3]={0};
+        // Node* temp=head;
+        // while(temp!=nullptr){
+        //     hash[temp->data]++;
+        //     temp=temp->next;
+        // }
+        
+        // int i=0;
+        // temp=head;
+        // while(temp!=nullptr){
+        //     if(hash[i]>0){
+        //         temp->data=i;
+        //         temp=temp->next;
+        //         hash[i]--;
+        //         if(hash[i]==0){
+        //             i++;
+        //         }
+        //     }
+        //     else{
+        //         i++;
+        //     }
+        // }
+        // return head;
+        
+        
+        
+        
+        //Using merge Sort on Linked List to sort the list.
+        if(head==nullptr || head->next==nullptr){
+            return head;
         }
         
-        int i=0;
-        temp=head;
-        while(temp!=nullptr){
-            if(hash[i]>0){
-                temp->data=i;
-                temp=temp->next;
-                hash[i]--;
-                if(hash[i]==0){
-                    i++;
-                }
-            }
-            else{
-                i++;
-            }
-        }
-        return head;
+        Node *middleNode=findMiddle(head);
+        Node *rightHead= middleNode->next;
+        middleNode->next=nullptr;
+        Node *leftHead=head;
+        
+        leftHead=segregate(leftHead);
+        rightHead=segregate(rightHead);
+        
+        return mergedList(leftHead, rightHead);
     }
 };
 
